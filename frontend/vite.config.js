ede6fe2ccfd07c1fac7b84213d3761c6155b288d
uzma kousar <157550914+uzmakousaruzma@users.noreply.github.com>
2025-12-18 12:18:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://assessforge-2.onrender.com',
+        target: 'https://assessforge-3.onrender.com',
         changeOrigin: true,
       },
     },
